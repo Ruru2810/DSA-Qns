05a25869e7cd16df444919bd9d249ad00b366e69
Runak Das <runakdas00067@gmail.com>
2026-07-03 11:01:03 +0530

    Time: 1 ms (99.98%), Space: 46.8 MB (81.71%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length<2){
+            return nums[0];
+        }
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            result^=nums[i];
+        }
+        return result;
+    }
+}
