79e11bde9ae879c741a868abd36bd6cc534ade3f
Runak Das <runakdas00067@gmail.com>
2026-07-03 17:34:39 +0530

    Time: 0 ms (100%), Space: 48.2 MB (34.08%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long binary=n;
+        if(n<0){
+            x=1/x;
+            binary=-binary;
+        }
+        double ans=1;
+        while(binary>0){
+            if(binary%2==1){
+                ans*=x;
+            }
+            x*=x;
+            binary/=2;
+        }
+        return ans;
     }
 }
