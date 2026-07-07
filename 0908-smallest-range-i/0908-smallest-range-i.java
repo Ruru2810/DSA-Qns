e7705f14e256de9a4073c81113cdaddc3438d704
Runak Das <runakdas00067@gmail.com>
2026-07-07 08:37:06 +0530

    Time: 2 ms (81.38%), Space: 47.1 MB (77.23%) - LeetHub

---
+++ 0908-smallest-range-i/0908-smallest-range-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        int max=nums[0];
+        int min=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){max=nums[i];}
+            if(nums[i]<min){min=nums[i];}
+        }
+        int out=(max-k)-(min+k);
+        if(out<0){return 0;}
+        return out;
+    }
+}
