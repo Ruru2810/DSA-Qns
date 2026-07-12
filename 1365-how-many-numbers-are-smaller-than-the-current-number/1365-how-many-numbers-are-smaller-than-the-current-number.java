be281c2b5d251559c6587e2da21288d9bcdc1546
Runak Das <runakdas00067@gmail.com>
2026-07-12 20:52:02 +0530

    Time: 1 ms (99.89%), Space: 45.4 MB (68.78%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,15 +1,42 @@
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+//         int arr[]=new int[nums.length];
+//         for(int i=0;i<nums.length;i++){
+//             int count=0;
+//             for(int j=0;j<nums.length;j++){
+//                 if(j!=i && nums[j]<nums[i]){
+//                     count++;
+//                 }
+//                 arr[i]=count;
+//             }
+//         }
+//         return arr;
+//     }
+// }
+
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int arr[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(j!=i && nums[j]<nums[i]){
-                    count++;
-                }
-                arr[i]=count;
-            }
+        // Step 1: Count the frequency of each number (assuming constraints: 0 <= nums[i] <= 100)
+        int[] count = new int[102];
+        for (int num : nums) {
+            count[num]++;
         }
-        return arr;
+        
+        // Step 2: Transform the count array into a running sum of smaller elements
+        int runningSum = 0;
+        for (int i = 0; i < count.length; i++) {
+            int currentFreq = count[i];
+            count[i] = runningSum;
+            runningSum += currentFreq;
+        }
+        
+        // Step 3: Build the result array using our precalculated counts
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = count[nums[i]];
+        }
+        
+        return result;
     }
 }
