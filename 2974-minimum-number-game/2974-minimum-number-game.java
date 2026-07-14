6d8a7e6546fa263af645a5c062e2a339e5ce1825
Runak Das <runakdas00067@gmail.com>
2026-07-14 15:46:38 +0530

    Time: 6 ms (32.48%), Space: 47.1 MB (57.59%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,27 +1,45 @@
+// class Solution {
+//     public int[] numberGame(int[] nums) {
+
+//         int[] count = new int[101];
+//         int[] rez = new int[nums.length];
+//         int idx = 0;
+//         for (int num : nums) {
+//             count[num]++;
+//         }
+//         for (int num = 1; num < 101; num++) {
+//             while (count[num] > 0) {
+//                 int first = num;
+//                 count[num]--;
+//                 while (count[num] == 0) {
+//                     num++;
+//                 }
+//                 int second = num;
+//                 count[num]--;
+
+//                 rez[idx++] = second;
+//                 rez[idx++] = first;
+//             }
+//         }
+
+//         return rez;
+//     }
+// }
+
+import java.util.Arrays;
+
 class Solution {
     public int[] numberGame(int[] nums) {
-
-        int[] count = new int[101];
-        int[] rez = new int[nums.length];
-        int idx = 0;
-        for (int num : nums) {
-            count[num]++;
+        // Step 1: Sort the array in ascending order
+        Arrays.sort(nums);
+        
+        // Step 2: Swap adjacent elements (step by 2)
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            int temp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = temp;
         }
-        for (int num = 1; num < 101; num++) {
-            while (count[num] > 0) {
-                int first = num;
-                count[num]--;
-                while (count[num] == 0) {
-                    num++;
-                }
-                int second = num;
-                count[num]--;
-
-                rez[idx++] = second;
-                rez[idx++] = first;
-            }
-        }
-
-        return rez;
+        
+        return nums;
     }
 }
