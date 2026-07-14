2dd8d231c66fd72788fb90bcf9e5a1af184330a4
Runak Das <runakdas00067@gmail.com>
2026-07-14 15:41:30 +0530

    Time: 1 ms (100%), Space: 47.4 MB (86.18%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,14 +1,39 @@
+// class Solution {
+//     public String firstPalindrome(String[] words) {
+//         for(int i=0;i<words.length;i++){
+//             String rev="";
+//             for(int j=words[i].length()-1;j>=0;j--){
+//                 rev=rev+words[i].charAt(j);
+//             }
+//             if(words[i].equals(rev)){
+//                 return rev;
+//             }
+//         }
+//         return "";
+//     }
+// }
+
+
 class Solution {
     public String firstPalindrome(String[] words) {
-        for(int i=0;i<words.length;i++){
-            String rev="";
-            for(int j=words[i].length()-1;j>=0;j--){
-                rev=rev+words[i].charAt(j);
-            }
-            if(words[i].equals(rev)){
-                return rev;
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
             }
         }
         return "";
+    }
+    public boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
