49ee897ba97a0eb55515b1c444cc1f0af2f9ed78
Runak Das <runakdas00067@gmail.com>
2026-07-16 13:16:19 +0530

    Time: 0 ms (100%), Space: 43.3 MB (54.35%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public int scoreOfString(String s) {
         int diff = 0;
         for (int i = 0; i < s.length() - 1; i++) {
-            diff += Math.abs(s.charAt(i) - s.charAt(i + 1));
+            diff += Math.abs(s.charAt(i) - s.charAt(i + 1));//yaad rakkh lavde ki Math.abs -ve ko +ve mein kar kardeta hai i mean ki absolutte value mein convert kar deta haai  
         }
         
         return diff;
