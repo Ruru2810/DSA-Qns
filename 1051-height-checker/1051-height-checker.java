3c78e3f24a975fae892086a81cbafab09c00a38d
Runak Das <runakdas00067@gmail.com>
2026-07-23 09:28:59 +0530

    Time: 4 ms (80.61%), Space: 43.6 MB (29.36%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,7 +1,11 @@
 class Solution {
     public int heightChecker(int[] heights) {
         int count=0;
-        int arr[]=heights.clone();
+        int arr[]=new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            arr[i]=heights[i];
+        }
+        // int arr[]=heights.clone();// Lawde yeh same array ka replica banata hai utna bada function kyun banaya yaad rakh
         Arrays.sort(arr);
         for (int i=0;i<heights.length;i++){
             if(arr[i]!=heights[i]){
