0840e898e25f5c7bc8e3472cdb1dbf2bd402fd74
Runak Das <runakdas00067@gmail.com>
2026-07-25 09:37:50 +0530

    Time: 5 ms (35.23%), Space: 49.6 MB (5.59%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -1,9 +1,28 @@
+// class Solution {
+//     public String largestGoodInteger(String num) {
+//         String arr[]={"999","888","777","666","555","444","333","222","111","000"};
+//         for(int i=0;i<arr.length;i++){
+//             if(num.contains(arr[i])){return arr[i];}
+//         }
+//         return "";
+//     }
+// }
+
 class Solution {
     public String largestGoodInteger(String num) {
-        String arr[]={"999","888","777","666","555","444","333","222","111","000"};
-        for(int i=0;i<arr.length;i++){
-            if(num.contains(arr[i])){return arr[i];}
+        char max=' ';
+        int count=1;
+        for(int i=0;i<num.length()-1;i++){
+            if(num.charAt(i)==num.charAt(i+1)){
+                count++;
+            }else{
+                count=1;
+            }
+            if(count==3){
+                if(num.charAt(i)>max){max=num.charAt(i);}
+            }
         }
-        return "";
+        if(max==' '){return "";}
+        return ""+max+max+max;
     }
 }
