534cbb686ce3102391c88f0bd95f84eb28b3c7ee
Runak Das <runakdas00067@gmail.com>
2026-07-30 22:44:51 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.44%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        //int count=0;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]%2==nums[i+1]%2){
                 return false;
