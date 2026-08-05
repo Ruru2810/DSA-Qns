ed4b742ee5f46cca6a23847dc233e833cb7af339
Runak Das <runakdas00067@gmail.com>
2026-08-05 09:28:39 +0530

    Time: 85 ms (0.47%), Space: 42.7 MB (99.85%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,17 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
+
+// class Solution {
+//     public int[] createTargetArray(int[] nums, int[] index) {
+//         List<Integer> list = new ArrayList<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             list.add(index[i], nums[i]);
+//         }
+//         int[] target = new int[nums.length];
+//         for (int i = 0; i < nums.length; i++) {
+//             target[i] = list.get(i);
+//         }
+        
+//         return target;
+//     }
+// }
+
+
+//0% beat bc but kyunnnnnnnnnnnnnnnn 🤬🤬
+
 
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            list.add(index[i], nums[i]); // Automatically shifts elements right
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            a.add(index[i],nums[i]);
         }
-        int[] target = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            target[i] = list.get(i);
+        int target[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            target[i] = a.get(i);
         }
-        
         return target;
     }
 }
