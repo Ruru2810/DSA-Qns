b050cce7fe4da8e9c733e54dcb680540867814bd
Runak Das <runakdas00067@gmail.com>
2026-09-19 11:10:56 +0530

    Time: 9 ms (41.82%), Space: 41.7 MB (93.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,7 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) {return n;}
-        int t1 = 0;
-        int t2 = 1;
-        for (int i = 2; i <= n; i++) {
-            int next = t1 + t2;
-            t1 = t2;
-            t2 = next;
-        }
-        return t2;
+        if(n==0){return 0;}
+        if(n==1){return 1;}
+        return fib(n-1)+fib(n-2);
     }
 }
